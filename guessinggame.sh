4f90df0c4ba53f40706c7839d7a392a55c238b61
sidrao2006 <sidrao2006@gmail.com>
2020-06-12 18:02:30 +0530

    Added comments

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,31 @@
 function guessinggamme {
+    # Question (Asking for the first time)
     echo "How many files are there in this directory"
     read response
+    # A loop
+    # Using condition to check answer
     while [[ $response != 13 ]]
     do
         if [[ $response -gt 13 ]]
         then
+            # If the answer was too high
             echo "Sorry that's not it...your reponse was too high"
             echo
         elif [[ $response -lt 13 ]]
         then
+            # If the answer was too low
             echo "Sorry that's not it...your reponse was too low"
             echo
         fi
-
+        # Prompting the user again
         echo "No worries, why don't you give it another try?"
         read response
     done
 
+    # Loop complete
     echo
     echo "Hey congratulations!!! You got it right. 13 it is!"
 }
 
+# Calling function
 guessinggamme
